8f8891bc53355a55fd21edf5157651cfc67c0e0e
Maksim Kl <GuruHacker@yandex.ru>
2023-05-25 21:31:04 +0300

    Initial commit

---
+++ HomeWork-4.01.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <functional>
+
+int main()
+{
+    double angles[3] = { 30, 60, 90 };
+	for (auto& i : angles) {
+		i *= 3.1415926 / 180;
+	}
+	std::function<void(double)> functions[3] = {
+		[](const auto& ang) {
+			std::cout << "sin: " << sin(ang) << " ";
+			},
+		[](const auto& ang) {
+			std::cout << "cos: " << cos(ang) << " ";
+			},
+		[](const auto& ang) {
+			std::cout << "tan: " << tan(ang) << " ";
+			}
+	};
+
+	for (const auto& angle : angles) {
+		std::cout << angle << ": ";
+		for (const auto& function : functions)
+			function(angle);
+		std::cout << std::endl;
+	}
+}
